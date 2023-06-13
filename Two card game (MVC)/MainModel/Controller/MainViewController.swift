b4a161dca5d34003b2454b776ee06f2f9c98f15a
Pavelweb22 <pavelweb2016@yandex.ru>
2023-06-13 19:01:33 +0300

    preparing project

---
+++ Two card game (MVC)/MainModel/Controller/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Two card game (MVC)
+//
+//  Created by Павел Яковенко on 13.06.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setConstraints()
+    }
+
+    private func setupViews() {
+        view.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        
+        
+    }
+
+}
+
+//MARK: - setConstraints
+
+extension MainViewController{
+    
+    private func setConstraints() {
+        
+    }
+}
